f7b2264a897e362e6bdc123fa46b5b07595520b0
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-04-17 16:59:15 -0500

    working version

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-from pickle import TRUE
 import pyautogui as gui
 import pynput 
 from pynput.mouse import Listener as MouseListener
@@ -8,13 +7,10 @@ import pytesseract
 from time import sleep
 
 exiting = False
-
-path_to_tesseract = r"C:\Users\allenmik\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\allenmik\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
 mouseX, mouseY = 0, 0
 
-tailsbox1X, tailsbox1Y = 0, 0
-tailsbox2X, tailsbox2Y = 0, 0
 headsbox1X, headsbox1Y = 0, 0
 headsbox2X, headsbox2Y = 0, 0
 
@@ -29,9 +25,10 @@ def getText(image_path):
     
     # Providing the tesseract executable
     # location to pytesseract library
-    pytesseract.tesseract_cmd = path_to_tesseract
+    # pytesseract.tesseract_cmd = path_to_tesseract
     
-    return pytesseract.image_to_string(img)
+    return pytesseract.image_to_string(img, lang='eng',
+           config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
 
 def exit():
     if exiting:
@@ -47,15 +44,28 @@ def loop():
     exit()
 
     # flipping
-    sleep(1)
     mouse.position = (flipboxX, flipboxY)
     sleep(1)
-    mouse.position = (flipboxX, flipboxY)
+    mouse.click(pynput.mouse.Button.left)
+    sleep(1)
+    mouse.click(pynput.mouse.Button.left)
 
     exit()
 
+    sleep(1)
+
     # reading heads
-    headimg = gui.screenshot("head.png", region=(headsbox1X,headsbox1Y, headsbox2X-headsbox1X, headsbox2Y- headsbox1Y))
+    gui.screenshot("head.png", region=(headsbox1X,headsbox1Y, headsbox2X-headsbox1X, headsbox2Y- headsbox1Y))
+    heads = int(getText("head.png") or "5") or 5
+
+    if heads > 6:
+        mouse.position = (cheaterboxX, cheaterboxY)
+    else:
+        mouse.position = (fairboxX, fairboxY)
+    sleep(.5)
+    mouse.click(pynput.mouse.Button.left)
+
+    sleep(3)
 
 
 # im = pyautogui.screenshot(region=(0,0, 300, 400))
@@ -72,6 +82,7 @@ ml = MouseListener(
 ml.start()
 
 def OnPress(key):
+    global exiting
     global checking
     global tailsbox1X, tailsbox1Y
     global tailsbox2X, tailsbox2Y
@@ -85,26 +96,21 @@ def OnPress(key):
     k = '{0}'.format(key)
     if (k == "'s'"):
         if checking == 0:
-            tailsbox1X, tailsbox1Y = mouseX, mouseY
-        elif checking == 1:
-            tailsbox2X, tailsbox2Y = mouseX, mouseY
-        elif checking == 2:
             headsbox1X, headsbox1Y = mouseX, mouseY
-        elif checking == 3:
+        elif checking == 1:
             headsbox2X, headsbox2Y = mouseX, mouseY
-        elif checking == 4:
+        elif checking == 2:
             cheaterboxX, cheaterboxY = mouseX, mouseY
-        elif checking == 5:
+        elif checking == 3:
             fairboxX, fairboxY = mouseX, mouseY
-        elif checking == 6:
+        elif checking == 4:
             flipboxX, flipboxY = mouseX, mouseY
-        checking = ( checking + 1 ) % 7
+        checking = ( checking + 1 ) % 5
     elif (k == "'l'"):
         print("Loop started")
         while True:
             loop()
 
-        checking = (checking + 1)%6
     elif (k == "'\\x03'"):
         exiting = True
         quit()
